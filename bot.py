1719fab176015426588522805f1850606d8f4eb4
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-05-25 19:31:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -451,10 +451,10 @@ async def badges(ctx, *, member: discord.Member = None):
 
 
 	e.set_footer(text = f"Member since: {member.joined_at.__format__('%d %b %Y at %H:%M:%S')}")#.timestamp = member.joined_at
-	e.add_field(name = 'Account created at', value = member.created_at.__format__('Date: **%d %b %Y**\nTime: **%H:%M:%S**'))
+	e.add_field(name = 'Account created at', value = member.created_at.__format__('Date: **%d %b %Y**\nTime: **%H:%M:%S**\nㅤ'))
 	e.add_field(name = 'User ID', value = member.id)
 	e.add_field(name = b1, value = b2, inline=True)
-	e.add_field(name = 'Pokébadges', value = f"{features}ㅤ{features1}ㅤ{features2}ㅤ{features3}ㅤ{features4}ㅤ{features5}\n\n{features6}ㅤ{features7}ㅤ{features8}ㅤ{features9}ㅤ{features10}ㅤ{features11}\n\n{features12}ㅤ{features13}ㅤ{features14}ㅤ{features15}ㅤ{features16}ㅤ[{features17}]")
+	e.add_field(name = 'Pokébadges', value = f"{features}ㅤ{features1}ㅤ{features2}ㅤ{features3}ㅤ{features4}ㅤ{features5}\n\n{features6}ㅤ{features7}ㅤ{features8}ㅤ{features9}ㅤ{features10}ㅤ{features11}\n\n{features12}ㅤ{features13}ㅤ{features14}ㅤ{features15}ㅤ{features16}ㅤ[{features17}]", inline = True)
 
 	await ctx.send(embed=e)
 
